87f849e3533f081f1a8aedd6178e12d51e39dc7a
Alfonso Aguilera <alfonaguilera@MacBook-Pro.local>
2024-06-05 17:20:37 +0200

    Import cors to fix ReferenceError

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
+import cors from "cors";
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
